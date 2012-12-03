986b1974054c8d57a81c456f94d79719b4e6ddc1
Nathan Boley <npboley@gmail.com>
2012-12-02 22:44:37 -0800

    Cahnged the gff parser to only return non coding elements.

--- utilities/gtf_from_gff3.py
+++ utilities/gtf_from_gff3.py
@@ -14,7 +14,7 @@ def main():
             if line.startswith( "#" ): continue
             data = line.split()
             try:
-                if data[2] == 'mRNA':
+                if data[2].endswith( 'RNA' ):
                     mRNAs.add( meta_data( data[8] )['ID'] )
                 elif data[2] == 'protein':
                     cds_transcripts.add( meta_data( data[8] )['Derives_from'] )
@@ -29,10 +29,10 @@ def main():
         for transcript_id, exons in transcripts.iteritems():
             if transcript_id not in mRNAs:
                 continue
-            strands = [ exon.split()[6] for exon in exons ]
-            if len( set( strands ) ) > 1: continue
-            #if transcript_id in cds_transcripts:
-            #    continue
+            if len( set( exon.split()[6] for exon in exons ) ) > 1: 
+                continue
+            if transcript_id in cds_transcripts:
+                continue
             for line in exons:
                 if line.startswith( "dmel_mitochondrion_genome" ):
                     continue
