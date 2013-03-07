10bc7de08e6f286e1f1d765f75c83786c6323e49
Nathan Boley <npboley@gmail.com>
2013-03-07 20:59:25 +0000

    Updated annotation insertion code to work with the new db format.

--- postgres_interface/add_annotation_to_db.py
+++ postgres_interface/add_annotation_to_db.py
@@ -9,11 +9,10 @@ import psycopg2
 def add_exons_to_db( transcript, conn ):
     cursor = conn.cursor()
     for (start, stop) in transcript.exons:
-        query = "INSERT INTO exons " \
-            + "( transcript, contig, strand, location )" \
-            + "VALUES ( '%s', '(\"%s\", 1)', '%s', '[%s, %s]')" % \
-            ( transcript.id, transcript.chrm, transcript.strand,
-              start, stop )
+        query = "INSERT INTO annotations.exons " \
+            + "( transcript, location )" \
+            + "VALUES ( '%s', '[%s, %s]')" % \
+            ( transcript.id, start, stop )
         cursor.execute( query )
     cursor.close()
     return
@@ -24,7 +23,7 @@ def add_transcript_regions_to_db( transcript, conn ):
     if transcript.is_protein_coding:
         start = transcript.relative_pos( transcript.cds_region[0] )
         stop = transcript.relative_pos( transcript.cds_region[1] )
-        query = "INSERT into transcript_regions " \
+        query = "INSERT into annotations.transcript_regions " \
               + "VALUES ( '%s', '[%s,%s]', '%s' )" \
               % (transcript.id, start, stop, 'CDS')
         cursor.execute( query )
@@ -32,12 +31,13 @@ def add_transcript_regions_to_db( transcript, conn ):
     cursor.close()
     return
 
-def add_transcript_to_db( gene, annotation_key, transcript, conn ):
+def add_transcript_to_db( gene_id, (chrm, assembly_id), transcript, conn ):
+    assert chrm == transcript.chrm
     cursor = conn.cursor()
-    query = "INSERT INTO transcripts " \
-        + "( id, gene, annotation, contig, strand, location )" \
-        + "VALUES ( '%s', '%s', %s, '(\"%s\", 1)', '%s', '[%s, %s]')" % \
-        ( transcript.id, gene.id, annotation_key, transcript.chrm, 
+    query = "INSERT INTO annotations.transcripts " \
+        + "( id, gene, contig, strand, location )" \
+        + "VALUES ( '%s', %s, '(\"%s\", %s)', '%s', '[%s, %s]')" % \
+        ( transcript.id, gene_id, chrm, assembly_id,
           transcript.strand, transcript.start, transcript.stop )
     cursor.execute( query )
     cursor.close()
@@ -48,23 +48,26 @@ def add_transcript_to_db( gene, annotation_key, transcript, conn ):
     
     return 
 
-def add_gene_to_db( gene, conn, annotation_key ):
+def add_gene_to_db( gene, annotation_key, assembly_id, conn ):
     cursor = conn.cursor()
     
     #add the gene entry
-    query = "INSERT INTO genes ( id, annotation, contig, strand, location ) " \
-        + "VALUES ( '%s', %s, '(\"%s\", 1)', '%s', '[%s, %s]')" % \
-        ( gene.id, annotation_key, 
-          gene.chrm, gene.strand, gene.start, gene.stop )
+    query_template = "INSERT INTO annotations.genes " \
+        + "( name, annotation, contig, strand, location ) " \
+        + "VALUES ( '%s', %s, '(\"%s\", %i)', '%s', '[%s, %s]') " \
+        + "RETURNING id;"
+    query = query_template % ( gene.id, annotation_key, gene.chrm, 
+                               assembly_id, gene.strand, gene.start, gene.stop )
     cursor.execute( query )
+    gene_id = int(cursor.fetchone()[0])
     cursor.close()
     
     for trans in gene.transcripts:
-        add_transcript_to_db( gene, annotation_key, trans, conn )
+        add_transcript_to_db( gene_id, (gene.chrm, assembly_id), trans, conn )
 
 def add_annotation_to_db( conn, name, description='NULL' ):
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO annotations ( name, description )"
+    cursor.execute("INSERT INTO annotations.annotations ( name, description )"
                    + "VALUES ( %s, %s ) RETURNING id", (name, description) )
     rv = cursor.fetchone()[0]
     cursor.close()
@@ -77,11 +80,14 @@ def parse_arguments():
     parser.add_argument( 'gtf', type=file, help='GTF file to load.')
 
     # TODO add code to determine this automatically
-    parser.add_argument( '--annotation-name', required=True, help='Database host. ' )
-        
+    parser.add_argument( '--annotation-name', required=True, 
+           help='Name of the annotation we are inserting. ' )
+    parser.add_argument( '--assembly-id', required=True, 
+           help='ID of the assembly this annotation is based upon.' )
+
     parser.add_argument( '--host', default='localhost', help='Database host. ' )
     parser.add_argument( '--db-name', default='rnaseq_data', 
-                         help='Database name. ' )
+                         help='Database to insert the data into. ' )
     
     parser.add_argument( '--verbose', '-v', default=False, action='store_true',\
                              help='Whether or not to print status information.')
@@ -92,10 +98,10 @@ def parse_arguments():
     
     conn = psycopg2.connect("dbname=%s host=%s" % ( args.db_name, args.host) )
 
-    return args.gtf, args.annotation_name, conn
+    return args.gtf, args.annotation_name, int(args.assembly_id), conn
 
 def main():
-    gtf_fp, ann_name, conn = parse_arguments()
+    gtf_fp, ann_name, assembly_id, conn = parse_arguments()
     
     # add the annotation, and return the pkey
     annotation_key = add_annotation_to_db( conn, ann_name )
@@ -106,7 +112,7 @@ def main():
     # add all of the genes to the DB
     for i, gene in enumerate(genes):
         print "(%i/%i)    Processing %s" % ( i+1, len(genes), gene.id )
-        add_gene_to_db( gene, conn, annotation_key )
+        add_gene_to_db( gene, annotation_key, assembly_id, conn )
     
     conn.commit()
     conn.close()
