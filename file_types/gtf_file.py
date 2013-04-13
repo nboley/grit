437965e8771f69e0a676dd3da5dc2e1eb7879074
Nathan Boley <npboley@gmail.com>
2013-04-13 12:36:33 -0700

    Added python gtf parser.

--- file_types/gtf_file.py
+++ file_types/gtf_file.py
@@ -1,14 +1,243 @@
 # Copyright (c) 2011-2012 Nathan Boley
 
-import sys
+import os, sys
 from genomic_intervals import GenomicInterval
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 import itertools
 
 GffLine = namedtuple( "GffLine", ["region", "feature", "score", 
                                   "source", "frame", "group"]  )
 GtfLine = namedtuple( "GtfLine", ["region", "gene_id", "trans_id", "feature", 
                                   "score", "source", "frame", "meta_data"])
+
+def partition_coding_and_utr_segments( exons, cds_start, cds_stop ):
+    """Split the exons into UTR and CDS exons.
+
+    """
+    # find the exon index the the start codon intersects
+    cds_start_i = [ i for i, (start, stop) in enumerate(exons) 
+                    if cds_start >= start and cds_start <= stop ]
+    assert len( cds_start_i ) == 1
+    cds_start_i = cds_start_i[0]
+    
+    # we start at the cds_start exon because we know this index must be >= 
+    assert cds_stop >= cds_start
+    cds_stop_i = [ i for i, (start, stop) in enumerate(exons[cds_start_i:]) 
+                   if cds_stop >= start and cds_stop <= stop ]
+    assert len( cds_stop_i ) == 1
+    cds_stop_i = cds_stop_i[0] + cds_start_i
+    
+    def mod_external_bndrys( exons, lower_bnd, upper_bnd ):
+        """If necessary, shrink the external boundaries"""
+        # if it's an empty set, there is nothing to be done
+        if len( exons ) == 0: return exons
+        exons[0] = ( max(exons[0][0], lower_bnd), exons[0][1] )
+        exons[-1] = ( exons[-1][0], min(upper_bnd, exons[-1][1] ) )
+        return exons
+    
+    cds_exons = mod_external_bndrys( 
+        list(exons[cds_start_i:cds_stop_i+1]), cds_start, cds_stop )
+    
+    us_utr_stop_i = cds_start_i if exons[cds_start_i][0] < cds_start \
+        else cds_start_i - 1
+    us_utr_exons = mod_external_bndrys(
+        list(exons[:us_utr_stop_i+1]), 1, cds_start-1)
+    
+    ds_utr_stop_i = cds_stop_i if exons[cds_stop_i][1] > cds_stop \
+        else cds_stop_i + 1
+    ds_utr_exons = mod_external_bndrys(
+        list(exons[ds_utr_stop_i:]), cds_stop+1, 1e100)
+    
+    return us_utr_exons, cds_exons, ds_utr_exons
+
+
+class Transcript( object ):
+    def __init__(self, trans_id, chrm, strand, exons, cds_region,
+                 gene_id=None, score=None, rpkm=None, rpk=None, promoter=None ):
+        self.gene_id = gene_id
+        self.id = trans_id
+        self.chrm = chrm
+        self.strand = strand
+        
+        self.score = score
+        self.rpkm = rpkm
+        self.rpk = rpk
+        
+        exon_bnds = list( itertools.chain( *exons ) )
+        self.exon_bnds = exon_bnds
+        self.start = exon_bnds[0]
+        self.stop = exon_bnds[-1]
+        assert self.start <= self.stop
+
+        self.exons = tuple(zip(exon_bnds[:-1:2], exon_bnds[1::2]))
+        self.introns = tuple([ (x+1, y-1) for x, y in 
+                               itertools.izip(exon_bnds[1:-2:2], 
+                                              exon_bnds[2:-1:2]) ])
+        
+        self.is_protein_coding = ( cds_region != None )
+        
+        self.cds_region = cds_region
+        self.start_codon = None
+        self.stop_codon = None
+        
+        self.cds_exons = None
+        self.fp_utr_exons = None
+        self.tp_utr_exons = None
+        self.us_exons = None
+        self.ds_exons = None
+        
+        self.promoter = promoter
+        if cds_region != None:
+            self.add_cds_region( cds_region )
+            
+    def add_cds_region( self, cds_region ):
+        self.cds_region = cds_region
+        self.us_exons, self.cds_exons, self.ds_exons = \
+            partition_coding_and_utr_segments( 
+            self.exons, self.cds_region[0], self.cds_region[1] )
+    
+        us_codon, ds_codon = self.cds_region[0], self.cds_region[1]
+        # if this is a reverse strand transcript, rev 5' and 3' ends
+        if self.strand == '+':
+            self.fp_utr_exons, self.tp_utr_exons \
+                = self.us_exons, self.ds_exons
+            self.start_codon, self.stop_codon = us_codon, ds_codon
+        else:
+            self.fp_utr_exons, self.tp_utr_exons \
+                = self.ds_exons, self.us_exons
+            self.stop_codon, self.start_codon = us_codon, ds_codon
+        
+    def __hash__( self ):
+        if self.cds_region != None:
+            return hash(( self.chrm, self.strand, 
+                          self.exons, tuple(self.cds_region) ))
+        else:
+            return hash( (self.chrm, self.strand, self.exons, None) )
+    
+    def IB_key( self ):
+        """Return a key for matching transcripts on their external bounds.
+        
+        """
+        if len( self.exon_bnds ) == 2:
+            return ( self.chrm, self.strand, "SE_GENE", self.cds_region )
+        else:
+            return (self.chrm, self.strand, 
+                    tuple(self.exon_bnds[1:-1]), self.cds_region)
+    
+    def relative_pos( self, genome_coord ):
+        """Convert a genme coordinate into a transcript coordinate.
+        
+        """
+        return genome_coord - self.start - sum( 
+            i_stop - i_start + 1 
+            for i_start, i_stop in self.introns 
+            if i_stop < genome_coord )
+
+    def genome_pos( self, trans_coord ):
+        """Convert a transcript coordinate into a genome coordinate.
+        
+        """
+        tot_len = 0
+        insert_len = 0
+        for (i_start, i_stop), (e_start, e_stop) \
+                in itertools.izip( self.introns, self.exons ):
+            e_len = e_stop - e_start + 1
+            if tot_len + e_len > trans_coord:
+                break
+            else:
+                insert_len += i_stop - i_start + 1
+        
+        return trans_coord + self.start + insert_len
+    
+    def build_gtf_lines( self, gene_id, meta_data, source='.'):
+        ret_lines = []
+        def build_lines_for_feature( exons, feature, is_CDS=False ):
+            current_frame = 0
+            score = str(self.score) if self.score != None else '.'
+            for start, stop in exons:
+                region = GenomicInterval( self.chrm, self.strand, start, stop )
+                frame = current_frame if is_CDS else '.'
+                yield create_gtf_line( region, gene_id, self.id, meta_data,
+                                       score, feature=feature, frame=str(frame),
+                                       source=source)
+                current_frame = ( current_frame + stop - start + 1 )%3
+            return
+        
+        ret_lines.extend( build_lines_for_feature( 
+                self.exons, 'exon', False ) )
+        
+        if self.cds_region != None:
+            us_exons, ds_exons = self.fp_utr_exons, self.tp_utr_exons
+            us_label, ds_label = 'five_prime_UTR', 'three_prime_UTR'
+            if self.strand == '-': 
+                us_exons, ds_exons = ds_exons, us_exons
+                us_label, ds_label = ds_label, us_label
+            
+            ret_lines.extend( build_lines_for_feature( 
+                    us_exons, us_label, False ) )
+            
+            ret_lines.extend( build_lines_for_feature( 
+                    self.cds_exons, 'CDS', True ) )
+            
+            ret_lines.extend( build_lines_for_feature( 
+                    ds_exons, ds_label, False ) )
+        
+        return "\n".join( ret_lines )
+
+def flatten( regions ):
+    new_regions = []
+    curr_start = regions[0][0]
+    curr_end = regions[0][1]
+    for i,(start,end) in enumerate(regions):
+        if curr_end > end:
+            end = curr_end
+        if i+1 == len( regions ): 
+            if len(new_regions) == 0:
+                new_regions = [curr_start, curr_end]
+                break
+            if new_regions[-1][1] == end:
+                break
+            else:
+                new_regions.append( [ curr_start, curr_end] )
+                break
+        if regions[i+1][0]-end <= 1:
+            curr_end = max( regions[i+1][1], end ) 
+        else:
+            new_regions.append( [curr_start, curr_end] )
+            curr_start = regions[i+1][0]
+            curr_end = regions[i+1][1]
+    if type(new_regions[0]) == int:
+        return [new_regions]
+    else:
+        return new_regions
+    
+class Gene( list ):
+    def __init__( self, id, chrm, strand, start, stop, transcripts ):
+        self.id = id
+        self.chrm = chrm
+        self.strand = strand
+        self.start = start
+        self.stop = stop
+        self.transcripts = transcripts
+        list.extend( self, ( id, chrm, strand, start, stop, transcripts ) )
+        return    
+
+    def find_transcribed_regions( self ):
+        exons = set()
+        for transcript in self.transcripts:
+            exons.update( transcript.exons )
+        
+        return flatten( sorted( exons ) )
+    
+    def calc_bpkm(self, read_cov):
+        base_cov, length = 0.0, 0
+        for start, stop in self.find_transcribed_regions():
+            length += stop - start + 1
+            base_cov += read_cov[(self.chrm, self.strand)][start:stop+1].sum()
+        
+        return base_cov/length
+        
+
 
 def parse_gff_line( line, fix_chrm=True ):
     data = line.split()
@@ -37,7 +266,7 @@ def parse_gff_line( line, fix_chrm=True ):
     
     # check the score
     if data[5] == '.':
-        data[5] = -1
+        data[5] = '.'
     else:
         try:
             data[5] = float(data[5])
@@ -59,7 +288,7 @@ def parse_gff_line( line, fix_chrm=True ):
     return GffLine( GenomicInterval(data[0], data[6], data[3], data[4]), \
                         data[2], data[5], data[1], data[7], data[8] )
 
-def parse_gtf_line( line, fix_chrm=True ):
+def parse_gtf_line( line, fix_chrm=True, use_name_instead_of_id=True ):
     gffl = parse_gff_line( line, fix_chrm=fix_chrm )
     if gffl == None: return None
     
@@ -83,14 +312,72 @@ def parse_gtf_line( line, fix_chrm=True ):
             name = name[:-1]
         return name
     
-    gene_name = meta_data[ 'gene_id' ]
+    if use_name_instead_of_id and 'gene_name' in meta_data:
+        gene_name = meta_data[ 'gene_name' ]
+    else:
+        gene_name = meta_data[ 'gene_id' ]
     gene_name = get_name_from_field( gene_name )
-    
-    trans_name = meta_data[ 'transcript_id' ]
+
+    if use_name_instead_of_id and 'transcript_name' in meta_data:
+        trans_name = meta_data[ 'transcript_name' ]
+    else:
+        trans_name = meta_data[ 'transcript_id' ]    
     trans_name = get_name_from_field( trans_name )
     
     return GtfLine( gffl[0], gene_name, trans_name, \
                         gffl[1], gffl[2], gffl[3], gffl[4], meta_data )
+
+def load_transcript_from_gtf_data(transcript_lines):
+    exons = []
+    scores = set()
+    rpk = None
+    rpkm = None
+    promoter = None
+    CDS_region = None
+    for line in transcript_lines:
+        scores.add( line.score )
+        if rpk == None and 'rpk' in line.meta_data: 
+            rpk = line.meta_data['rpk']
+        if rpkm == None and 'rpkm' in line.meta_data: 
+            rpkm = line.meta_data['rpkm']
+        
+        if line.feature == 'exon':
+            exons.append( (line.region.start, line.region.stop) )
+        elif line.feature == 'CDS':
+            CDS_region = (line.region.start, line.region.stop)
+        elif line.feature == 'promoter':
+            promoter = line.region
+    
+    if len( exons ) == 0:
+        return None
+    
+    score = next(iter(scores)) if len(scores) == 1 else None
+    line = transcript_lines[0]
+    return Transcript( line.trans_id, line.region.chr, line.region.strand, 
+                       sorted(exons), CDS_region,
+                       line.gene_id, score, rpkm, rpk, promoter )
+
+def load_gtf_file(fp, use_name_instead_of_id=False):
+    gene_lines = defaultdict(lambda: defaultdict(list))
+    for line in fp:
+        data = parse_gtf_line(line)
+        if None == data: continue
+        gene_lines[data.gene_id][data.trans_id].append(data)
+    
+    genes = []
+    for gene_id, transcripts_data in gene_lines.iteritems():
+        transcripts = []
+        for trans_id, transcript_lines in transcripts_data.iteritems():
+            transcript = load_transcript_from_gtf_data(transcript_lines)
+            if transcript == None: continue
+            transcripts.append(transcript)
+        t_1 = transcripts[0]
+        genes.append( Gene( gene_id, t_1.chrm, t_1.strand, 
+                            min(t.start for t in transcripts ), 
+                            max(t.stop for t in transcripts ), 
+                            transcripts ) )
+    
+    return genes
 
 def create_gff_line( region, grp_id, score=0, \
                      feature='.', source='.', frame='.' ):
@@ -183,3 +470,9 @@ def iter_gtf_lines( regions_iter, gene_id_iter, trans_id_iter,               \
         yield create_gtf_line( *data )
     
     return
+
+if __name__ == '__main__':
+    with open( sys.argv[1] ) as fp:
+        for gene in load_gtf_file( fp ):
+            for t in gene.transcripts:
+                print t.build_gtf_lines( gene.id, {} )
