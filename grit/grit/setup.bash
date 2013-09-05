cef4265cd4eb8b532e885af75c94c5496a3e7a22
Nathan Boley <npboley@gmail.com>
2013-09-05 15:01:21 -0700

    Moved the setup script

--- grit/grit/setup.bash
+++
@@ -1,5 +0,0 @@
-cython ./sparsify_support_fns.pyx
-gcc -shared -pthread -fPIC -fwrapv -O3 -Wall -fno-strict-aliasing \
-    -I/usr/include/python2.7 \
-    -o ./sparsify_support_fns.so \
-    ./sparsify_support_fns.c
