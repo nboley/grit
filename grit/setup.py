f91ac8728f4848fd0cd0124c9145f67952baa05b
Nathan Boley <npboley@gmail.com>
2014-03-21 11:11:43 -0700

    Cleaned up the setup script to include the pyx file, and better deal with compilation.

--- grit/setup.py
+++ grit/setup.py
@@ -1,41 +1,37 @@
 #from distutils.core import setup
 #from distutils.extension import Extension
 
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
 try: import Cython
 except ImportError: USE_CYTHON = False
 else: USE_CYTHON = True
 
-# cython seems to be broken on some architecture, so just use
-# the c file until I can resolve this
-USE_CYTHON = False
-ext = 'pyx' if USE_CYTHON else 'c'
-
-extensions = [
-    Extension("sparsify_support_fns", ["grit/sparsify_support_fns." + ext, ])
-]
+# if we have cython, then build the pyx file
 if USE_CYTHON:
     from Cython.Build import cythonize
-    extensions = cythonize(extensions)
+    extensions = cythonize("grit/sparsify_support_fns.pyx")
+else:
+    extensions = [
+        Extension("sparsify_support_fns", ["grit/sparsify_support_fns.c", ])
+    ]
+
 
 config = {
+    'include_package_data': True,
     'ext_modules': extensions,
     'description': 'GRIT',
     'author': 'Nathan Boley',
     'url': 'http://grit-bio.org/',
     'download_url': 'http://grit-bio.org/git/',
     'author_email': 'npboley@gmail.com',
-    'version': '1.1.1',
+    'version': '1.1.2-dev',
     'packages': ['grit', 
                  'grit.analyze', 
                  'grit.files', 
                  'grit.lib', 
-                 'grit.proteomics', 
-                 'grit.random_forest', 
-                 'grit.simulator'],
-                 
-    'setup_requires': [ 'scipy', 'numpy', 'Cython', 'networkx', 'pysam' ],
+                 'grit.proteomics'],
+    'setup_requires': [ 'Cython', ],
     'install_requires': [ 'scipy', 'numpy', 'Cython', 'networkx', 'pysam' ],
     'scripts': ['./bin/run_grit.py',],
     'name': 'GRIT'
