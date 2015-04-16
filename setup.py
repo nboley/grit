527dadcfb7c6212532e84ff4740e01aa744f35e5
Nathan Boley <npboley@gmail.com>
2015-04-15 17:36:58 -0700

    Changed to version 2.0.3

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ config = {
     'url': 'http://grit-bio.org/',
     'download_url': 'http://grit-bio.org/git/',
     'author_email': 'npboley@gmail.com',
-    'version': '2.0.3BETA',
+    'version': '2.0.3',
     'packages': ['grit', 
                  'grit.analyze', 
                  'grit.files', 
